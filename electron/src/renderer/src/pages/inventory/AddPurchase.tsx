0bd5c5bda72ebf204afe138fd2062374f81e5cd2
ch-hamid14 <hamidashraf3440@gmail.com>
2026-07-14 16:31:50 +0500

    set the purchase price and retail price is same issue

--- electron/src/renderer/src/pages/inventory/AddPurchase.tsx
+++ electron/src/renderer/src/pages/inventory/AddPurchase.tsx
@@ -179,6 +179,7 @@ export const AddPurchase = () => {
           productId: l.productId,
           colorId: l.colorId,
           purchasePrice: l.purchasePrice,
+          sellingPrice: l.listPrice,
           warrantyActive: l.warrantyActive,
           warrantyExpiryDate: l.warrantyExpiryDate
         }))
